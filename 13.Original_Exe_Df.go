95ca70e6130f304d9f071230781594da928ed9c5
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-08-19 18:15:17 +0530

    Create 13.Original_Exe_Df.go

---
+++ 13.Original_Exe_Df.go
@@ -0,0 +1,9 @@
+# ============================================================
+# CELL 13: ORIGINAL EXECUTION DATASET
+# ============================================================
+
+original_df = pd.DataFrame(
+    original_results
+)
+
+display(original_df)
